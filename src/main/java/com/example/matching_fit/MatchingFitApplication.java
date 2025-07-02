9f47f366b07b642752e2cdf586f54cfaf217bcdf
gwon tae-yun <taeyun1118@naver.com>
2025-07-02 14:57:28 +0900

    first commit

---
+++ src/main/java/com/example/matching_fit/MatchingFitApplication.java
@@ -0,0 +1,13 @@
+package com.example.matching_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/matching_fit/MatchingFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matching_fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
